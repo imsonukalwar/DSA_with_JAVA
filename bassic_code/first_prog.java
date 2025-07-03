80f0a5deb92d4c1e6aae44cca1dd2ad8b8313182
unknown <imsonukalwar@gmail.com>
2025-07-03 22:03:17 +0530

    change

---
+++ bassic_code/first_prog.java
@@ -0,0 +1,7 @@
+public class first_prog {
+      public static void main(String[] var0) {
+      System.out.println("Welcome");
+      System.out.println("sonu");
+      System.out.println("Coding");
+   }
+}
