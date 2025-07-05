4b782471c518b79b14427a83736a1226b2530da1
unknown <imsonukalwar@gmail.com>
2025-07-05 17:14:26 +0530

    new file

---
+++ bassic_math/febonachi.java
@@ -0,0 +1,16 @@
+public class febonachi {
+    public static void main(String[] args) {
+                int a=0;
+        int b=1;
+        int temp;
+        System.out.println(a);
+        System.out.println(b );
+        for (int i = 0; i < 10; i++) {
+        int c=a+b;
+        temp=a;
+        a=b;
+        b=c;
+System.out.println(c);
+        }
+    }
+}
