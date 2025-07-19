073c2cf925e5cf4bf9bd376e9fc523a37dcc82e9
unknown <imsonukalwar@gmail.com>
2025-07-20 00:13:11 +0530

    apload

--- bassic_math/first_prog.java
+++
@@ -1,7 +0,0 @@
-public class first_prog {
-      public static void main(String[] var0) {
-      System.out.println("Welcome");
-      System.out.println("sonu");
-      System.out.println("Coding");
-   }
-}
