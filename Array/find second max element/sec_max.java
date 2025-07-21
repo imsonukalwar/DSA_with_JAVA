19eb990e560a6a6abc65c7614033c28b8252bffb
unknown <imsonukalwar@gmail.com>
2025-07-21 23:28:05 +0530

    new code file

---
+++ Array/find second max element/sec_max.java
@@ -0,0 +1,26 @@
+public class sec_max {
+    public static int se_max(int a[]){
+        int max1=print_max(a);
+        int sec_max=a[0];
+        for (int i = 0; i <a.length; i++) {
+        if(a[i]>sec_max&&a[i]!=max1){
+                sec_max=a[i];
+            }
+        }
+        return sec_max;
+    }
+    public static int print_max(int a[]){
+        int max=a[0];
+        for (int i = 0; i <a.length; i++) {
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int a[]={2,4,6,8,5,1,4,9,};
+        int ans=se_max(a);
+        System.out.println(ans);
+    }
+}
