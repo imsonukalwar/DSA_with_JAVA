624d094883231a00b6674635a9c6033e64867861
unknown <imsonukalwar@gmail.com>
2025-08-08 11:30:25 +0530

    new code file

---
+++ Array/pluse one on array last element/solution2.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class solution2 {
+    public static int[] plusOne(int[] digits) {
+        int carry = 1;
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10;
+            carry = sum / 10;
+        }
+
+        if (carry == 1) {
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            return result;
+        }
+
+        return digits;
+    }
+
+    public static void main(String[] args) {
+            int[] digits = {9, 9, 9};
+        int[] result =plusOne(digits);
+        System.out.println("Result: " + Arrays.toString(result));
+        
+    }
+}
