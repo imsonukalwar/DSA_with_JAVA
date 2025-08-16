99b7847c1c1423d77e7144c924d844e086f64efb
unknown <imsonukalwar@gmail.com>
2025-08-16 07:56:18 +0530

    add new array code

---
+++ Array/intersection of two array/solution1/sol.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class sol{
+    public static int[] intersection(int nums1[],int nums2[]){
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] != -1 && nums1[i] == nums2[j]) {
+                    list.add(nums1[i]);
+                    nums2[j] = -1;
+                    break;
+                }
+            }
+        }
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int a1[]={1,4,4,8,4};
+        int a2[]={1,4,6,8,8};
+        int[] ans=intersection(a1, a2);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+}
