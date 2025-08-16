c64ed07b3053c9156db994b999e0d98e6755b964
unknown <imsonukalwar@gmail.com>
2025-08-16 09:24:42 +0530

    new array code

--- Array/intersection of two array/solution1/sol.java
+++ Array/intersection of two array/solution1/sol.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class sol{
     public static int[] intersection(int nums1[],int nums2[]){
         ArrayList<Integer> list = new ArrayList<>();
-
         for (int i = 0; i < nums1.length; i++) {
             for (int j = 0; j < nums2.length; j++) {
                 if (nums2[j] != -1 && nums1[i] == nums2[j]) {
