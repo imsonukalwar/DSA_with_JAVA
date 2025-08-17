e9dad7da0cfc0c6d37f0538c44784e080ae97be9
unknown <imsonukalwar@gmail.com>
2025-08-17 10:36:26 +0530

    maximum subarray sum

---
+++ Array/Maximum Subarray Sum in an Array/better solution/sol.java
@@ -0,0 +1,18 @@
+public class sol {
+    public static int maxsum(int a[]){
+        int max=0;
+        for (int i = 0; i < a.length; i++) {
+            int sum=0;
+            for (int j = i; j <a.length; j++) {
+                    sum+=a[j];
+                    max=Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int a[]={-2,-3,4,-1,-2,1,5,-3};
+        int ans=maxsum(a);
+        System.out.println(ans);
+    }
+}
